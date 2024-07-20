b8a9ec2533ccb55753fd98f77efe638d83710b2e
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-07-20 11:35:01 +0000

    fileUpdate

--- src/components/movies.js
+++ src/components/movies.js
@@ -4,35 +4,44 @@ import React, { useState } from 'react';
 function Movies() {
     const [input, setInput] = useState('');
     const [movies, setMovies] = useState([]);
+    const [error, setError] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setError('');
         axios.get(`http://www.omdbapi.com/?apikey=99eb9fd1&s=${input}`)
             .then((res) => {
-                setMovies(res.data.Search);
+                if (res.data.Response === "True") {
+                    setMovies(res.data.Search);
+                } else {
+                    setMovies([]);
+                    setError('Invalid movie name. Please try again.');
+                }
             }).catch((error) => {
-                console.log(error)
-            })
+                console.log(error);
+                setError('An error occurred. Please try again later.');
+            });
     }
 
     return (
         <>
-            <form onClick={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <input type='text' value={input} onChange={(e) => setInput(e.target.value)} />
                 <button type='submit'>Search</button>
             </form>
             <ul>
-                {movies && movies.length > 0 ? 
+                {movies.length > 0 ? 
                 (movies.map((movie) => (
                     <li key={movie.imdbID}>
                         <h1>{movie.Title}</h1>
                         <img src={movie.Poster} alt={`${movie.Title} Poster`} />
                     </li>
-                )
-            )) : (<p>Invalid movie name. Please try again.{movie.error}</p>)}
+                ))) : (
+                    <p>{error}</p>
+                )}
             </ul>
         </>
-    )
+    );
 }
 
 export default Movies;
